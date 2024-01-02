af968847f401243c9eb85c1b674e7183229cb30f
Juliette117 <ajuliette7@gmail.com>
2024-01-02 16:38:58 +0100

    feat/ajout exercice 1

---
+++ src/main/java/Exercices/TP1/Moyenne.java
@@ -0,0 +1,39 @@
+package Exercices.TP1;
+
+public class Moyenne {
+
+    public static double getMoyenne(double[] notes) throws IllegalAccessException {
+
+        verifierSiNotesComprisesEntreOet20(notes);
+
+        return calculerMoyenne(notes);
+    }
+
+    /**
+     * Calcule la moyenne des notes saisies
+     * @param notes
+     * @return double // La moyenne de l'élève
+     */
+    private static double calculerMoyenne(double[] notes) {
+        double moyenne =0;
+        for(double note: notes) {
+            moyenne += note;
+        }
+
+        return moyenne / notes.length;
+    }
+
+    private static void verifierSiNotesComprisesEntreOet20(double[] notes) throws IllegalArgumentException {
+        for (double note: notes) {
+            if(note < 0) {
+                throw new IllegalArgumentException("L'éleve ne peut pas avoir une note en dessous de 0");
+            }
+            else if(note > 20) {
+                throw new IllegalArgumentException("L'éleve ne peut pas avoir plus de 20");
+            }
+        }
+
+    }
+
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import Exercices.TP1.Moyenne;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
+
+        double[] notes = { 11, 16, 19, 13 };
+
+        System.out.println(Moyenne.getMoyenne((notes)));
 
 
     }
