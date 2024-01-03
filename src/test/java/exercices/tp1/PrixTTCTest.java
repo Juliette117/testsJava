486840661beae0f1f6a720ffe0578bc089804931
Juliette117 <ajuliette7@gmail.com>
2024-01-03 13:30:56 +0100

    feat/ajout tests exercice 2

--- src/test/java/exercices/tp1/PrixTTCTest.java
+++ src/test/java/exercices/tp1/PrixTTCTest.java
@@ -4,27 +4,39 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrixTTCTest {
 
-   // int prixHt = { };
+    //Arrange
+    PrixTTC prixTTC = new PrixTTC();
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "Test {index} : {0} * 1.20 = {1}")
+    //Données prix HT et prix TTC (expected) au format CSV
     @CsvSource({
             "10,12",
             "20,24",
             "100,120"
         })
-    @DisplayName("Test prix TTC")
+    @DisplayName("Test prix HT et TTC")
     public void testPrixTTC(int prixHT, int expected) {
-
-        PrixTTC prixTTC = new PrixTTC();
         //Act
         double result = prixTTC.HtToTtc(prixHT);
         //Assert
         assertEquals(expected, result);
     }
+
+    @ParameterizedTest
+    @DisplayName("Test vérification nombre négatif")
+    @ValueSource(ints = {10, 20, 100})
+    public void testVerifierPrixHT(int prixHT) {
+        assertThrows(IllegalArgumentException.class, () -> prixTTC.verifierPrix(prixHT));
+    }
+
+
+
+
 
 }
