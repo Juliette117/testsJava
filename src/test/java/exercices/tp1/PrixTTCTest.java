4873d8e5246eb130e0e1bfcea13eca7461eac283
Juliette117 <ajuliette7@gmail.com>
2024-01-03 12:11:58 +0100

    feat/ajout tests exercice 2

--- src/test/java/exercices/tp1/PrixTTCTest.java
+++ src/test/java/exercices/tp1/PrixTTCTest.java
@@ -1,7 +1,30 @@
-package Exercices.TP1;
+package exercices.tp1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrixTTCTest {
+
+   // int prixHt = { };
+
+    @ParameterizedTest
+    @CsvSource({
+            "10,12",
+            "20,24",
+            "100,120"
+        })
+    @DisplayName("Test prix TTC")
+    public void testPrixTTC(int prixHT, int expected) {
+
+        PrixTTC prixTTC = new PrixTTC();
+        //Act
+        double result = prixTTC.HtToTtc(prixHT);
+        //Assert
+        assertEquals(expected, result);
+    }
 
 }
