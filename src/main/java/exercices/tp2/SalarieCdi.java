6cbcc44e31e148e04fbf6757b88d810a501930ac
Juliette117 <ajuliette7@gmail.com>
2024-01-04 13:13:07 +0100

    feat/ajout classe SalarieCdi

---
+++ src/main/java/exercices/tp2/SalarieCdi.java
@@ -0,0 +1,12 @@
+package exercices.tp2;
+
+public class SalarieCdi extends Salarie {
+
+    public SalarieCdi(Salaire salaire, Identite identite) {
+        super(salaire, identite);
+    }
+
+
+
+
+}
