10a091eabc56717b8bb9b892f5188d6044b5063e
Juliette117 <ajuliette7@gmail.com>
2024-01-05 10:49:40 +0100

    feat/ajout classe Adresse

--- src/main/java/exercices/tp2/Adresse.java
+++ src/main/java/exercices/tp2/Adresse.java
@@ -7,4 +7,54 @@ public class Adresse {
     private int cp;
     private String ville;
     private String pays;
+
+
+
+    public Adresse(String voie, int rue, int cp, String ville, String pays) {
+        this.voie = voie;
+        this.rue = rue;
+        this.cp = cp;
+        this.ville = ville;
+        this.pays = pays;
+    }
+
+    public String getVoie() {
+        return voie;
+    }
+
+    public void setVoie(String voie) {
+        this.voie = voie;
+    }
+
+    public int getRue() {
+        return rue;
+    }
+
+    public void setRue(int rue) {
+        this.rue = rue;
+    }
+
+    public int getCp() {
+        return cp;
+    }
+
+    public void setCp(int cp) {
+        this.cp = cp;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
 }
